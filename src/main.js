aef7eb486ac002e61ca7de7a22f5ca1e5c5c1c51
admin <1463072498@qq.com>
2019-07-01 12:09:38 +0800

    项目登录页

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+// 引入element-ui
+import ElementUI from 'element-ui'
+// 引入element-ui的css文件
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/index.css'
+// 安装element
+Vue.use(ElementUI)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
